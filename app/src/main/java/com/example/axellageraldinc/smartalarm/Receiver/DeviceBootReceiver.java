e7dfc1de99be96dedf3828c38b435e9c2fdf0cbe
Almantera <alman.alfaruq@gmail.com>
2016-12-26 23:31:15 +0700

    Edit boot receiver biar jalanin background service waktu booting finish

--- app/src/main/java/com/example/axellageraldinc/smartalarm/Receiver/DeviceBootReceiver.java
+++ app/src/main/java/com/example/axellageraldinc/smartalarm/Receiver/DeviceBootReceiver.java
@@ -2,6 +2,7 @@ package com.example.axellageraldinc.smartalarm.Receiver;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
@@ -19,16 +20,25 @@ public class DeviceBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+//
+//            /* Setting the alarm here */
+//            Intent alarmIntent = new Intent(context, AlarmReceiver.class);
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+//
+//            AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+//            int interval = 8000;
+//            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+//
+//            Toast.makeText(context, "Alarm Set", Toast.LENGTH_SHORT).show();
 
-            /* Setting the alarm here */
-            Intent alarmIntent = new Intent(context, AlarmReceiver.class);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
-
-            AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-            int interval = 8000;
-            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
-
-            Toast.makeText(context, "Alarm Set", Toast.LENGTH_SHORT).show();
+            Intent mServiceIntent = new Intent();
+            mServiceIntent.setAction("com.example.axellageraldinc.smartalarm.BackgroundService");
+            ComponentName service = context.startService(mServiceIntent);
+            if (null == service) {
+                Toast.makeText(context, "Ga bisa", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, "Alarm set", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }

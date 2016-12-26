4f1cb96bea0ef35701534a3ae3ca9d405194d9b4
Almantera <alman.alfaruq@gmail.com>
2016-12-26 20:30:14 +0700

    Set check kalo repeat bukan "Don't repeat"

--- app/src/main/java/com/example/axellageraldinc/smartalarm/CustomRepeat.java
+++ app/src/main/java/com/example/axellageraldinc/smartalarm/CustomRepeat.java
@@ -27,6 +27,42 @@ public class CustomRepeat extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_repeat);
         findView();
+        ArrayList<Integer> intDaysOfWeek = getIntent().getIntegerArrayListExtra("daysOfWeek");
+        if (!intDaysOfWeek.isEmpty() || intDaysOfWeek != null) {
+            daysOfWeek = intDaysOfWeek;
+            chkMinggu.setChecked(daysOfWeek.contains(1));
+            chkSenin.setChecked(daysOfWeek.contains(2));
+            chkSelasa.setChecked(daysOfWeek.contains(3));
+            chkRabu.setChecked(daysOfWeek.contains(4));
+            chkKamis.setChecked(daysOfWeek.contains(5));
+            chkJumat.setChecked(daysOfWeek.contains(6));
+            chkMinggu.setChecked(daysOfWeek.contains(7));
+        }
+        checkListener();
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                Collections.sort(daysOfWeek);
+                intent.putIntegerArrayListExtra("daysOfWeek", daysOfWeek);
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    private void findView() {
+        btnOK = (Button) findViewById(R.id.btnOK);
+        chkSenin = (CheckBox) findViewById(R.id.chkSenin);
+        chkSelasa = (CheckBox) findViewById(R.id.chkSelasa);
+        chkRabu = (CheckBox) findViewById(R.id.chkRabu);
+        chkKamis = (CheckBox) findViewById(R.id.chkKamis);
+        chkJumat = (CheckBox) findViewById(R.id.chkJumat);
+        chkSabtu = (CheckBox) findViewById(R.id.chkSabtu);
+        chkMinggu = (CheckBox) findViewById(R.id.chkMinggu);
+    }
+
+    private void checkListener() {
         chkMinggu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -76,27 +112,6 @@ public class CustomRepeat extends AppCompatActivity {
                 else if (!b && daysOfWeek.contains(7)) daysOfWeek.remove(Integer.valueOf(7));
             }
         });
-        btnOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                Collections.sort(daysOfWeek);
-                intent.putIntegerArrayListExtra("daysOfWeek", daysOfWeek);
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            }
-        });
-    }
-
-    private void findView() {
-        btnOK = (Button) findViewById(R.id.btnOK);
-        chkSenin = (CheckBox) findViewById(R.id.chkSenin);
-        chkSelasa = (CheckBox) findViewById(R.id.chkSelasa);
-        chkRabu = (CheckBox) findViewById(R.id.chkRabu);
-        chkKamis = (CheckBox) findViewById(R.id.chkKamis);
-        chkJumat = (CheckBox) findViewById(R.id.chkJumat);
-        chkSabtu = (CheckBox) findViewById(R.id.chkSabtu);
-        chkMinggu = (CheckBox) findViewById(R.id.chkMinggu);
     }
 
     @Override
